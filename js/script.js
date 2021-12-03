8edaa10e60209cbdbfd64b42620ff3053681b0dc
mamba0328 <valentindjejylej@gmail.com>
2021-12-03 21:46:12 +0200

    fully functional project, some work is still neded

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-//creating div's grid 
-const container = document.getElementById('container')
+// creates custom grid and paints in it when mouseover
+const container = document.getElementById('container'); 
 
-
-function divAmount(num) { 
-
- for (i=1;i<=num; i++) {
-
-  const div = document.createElement('div')
-  div.classList.add('grid')
-  container.appendChild(div)
-
-  let s = 
-
-  //add hoover effect 
-  function changeColor(e) { 
-    e.target.style.backgroundColor = 'pink';
-  }
-  
-  div.addEventListener('mouseenter', changeColor)
-
+function pixelsAmount(num) {
+  if (container.children.length===0) {
+   let side = 960/num;
+   num*=num; 
+   for (i=1;i<=num;i++) { 
+    const div = document.createElement('div'); 
+    div.style.height =  side + 'px';
+    div.style.width = side + 'px';
+    div.style.float = 'left'
+    container.appendChild(div)
+    div.addEventListener('mouseover',changeColor)
+   } 
+ } else { 
+   console.log('WTF?')
  }
-
 }
 
-divAmount(16) 
+
+function changeColor (e) { 
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  e.target.style.backgroundColor = "#" + randomColor;
+}
+
+pixelsAmount(2);
 
 
+//clear whiteboard on reset click
 
+const reset = document.getElementById('reset');
+reset.addEventListener('click', clearBoard)
 
+function clearBoard () { 
+  container.innerHTML = '';
+  pixelsAmount(Math.floor(Math.random()*100));
+}
